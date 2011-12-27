20306c7fd4954347b8fad4bfd8a6b21277b4d412
Xifax <demi.log@gmail.com>
2011-12-27 18:01:25 +0400

    Package list updated

--- src/conf/const.py
+++ src/conf/const.py
@@ -3,7 +3,8 @@
 name = 'imus'
 version = '0.1'
 developer = 'Artiom Basenko'
-info = 'Scrobble that scrobbles.'
+info = 'Scrobbler that scrobbles.'
 # TODO:  Testing in progress, should select one of the hsaudio/mutagen libraries
-packages = ['redis', 'hiredis', 'hsaudiotag', 'mutagen']
+# TODO: ... the same goes for agrh/argparse, I guess
+packages = ['redis', 'hiredis', 'hsaudiotag', 'mutagen', 'argparse', 'argh']
 extensions = ['mp3', 'flac', 'mp4', 'mped', 'ogg', 'aiff']
