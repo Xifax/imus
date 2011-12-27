c13b73c052048f86e8f7861122f38324b6ef0ed6
Xifax <demi.log@gmail.com>
2011-12-27 13:49:12 +0400

    Fixed bugs, implemented little test

--- src/db/library.py
+++ src/db/library.py
@@ -6,16 +6,16 @@ from redis import StrictRedis
 
 class Track:
 
-    def __init__(self, name='',
+    def __init__(self, title='',
                        album='',
                        artist='',
                        folder=''):
-        self.name = name
+        self.title = title
         self.album = album
         self.artist = artist
         self.folder = folder
         colon = ':'
-        self.key = colon.join([self.name, self.album, self.artist, self.folder])
+        self.key = colon.join([self.title, self.album, self.artist, self.folder])
         self.data = {}
 
     def stats(self):
@@ -27,12 +27,26 @@ class Track:
         except ValueError:
             pass
 
+    def __repr__(self):
+        return "Title: %s | artist: %s | album: %s | location: %s" \
+                % (self.title, self.artist, self.album, self.folder)
+
     @staticmethod
     def from_redis(key, value):
         if key is not None:
-            track = Track(key.split(':'))
+            #track = Track(key.split(':'))
+            track = Track.arguments(key.split(':'))
             track.update_stats(value)
             return track
+
+    @staticmethod
+    def arguments(*args):
+        for arg in args:
+            title = arg.pop(0)
+            album = arg.pop(0)
+            artist = arg.pop(0)
+            folder = arg.pop(0)
+            return Track(title, album, artist, folder)
 
 class Redis:
 
@@ -46,7 +60,7 @@ class Redis:
         self.r.set(track.key, track.stats())
 
     def lookup(self, keyword):
-        return self.r.keys(keyword)
+        return self.r.keys('*' + keyword + '*')
 
     def retrieve(self, key):
         return Track.from_redis(key, self.r.get(key))
--- src/imus.py
+++ src/imus.py
@@ -1,1 +1,16 @@
+# -*- coding=utf-8 -*-
 
+from db.library import Track, Redis
+
+def main():
+    pass
+
+def test():
+    r = Redis()
+    r.update(Track('test', 'lalal', 'ohoho', 'oops'))
+    #print r.lookup('*')
+    print 'Lookup results ~ ', r.lookup('test')
+    print 'Recreated track ~ ', r.retrieve(r.lookup('test').pop())
+
+if __name__ == '__main__':
+    test()
